1e5e602d1cb1e82da8955c47ff17e0ffb6e5c388
Jonathan Merritt <j.s.merritt@gmail.com>
2021-08-14 22:31:15 +1000

    Setup API as a separate struct
    - API is now a struct full of functions (`WGPUHsInstance`).
    - The API is loaded from a dynamic library at runtime.

---
+++ wgpu-raw-hs/src/WGPU/Raw/Dynamic.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE CPP #-}
+
+-- |
+module WGPU.Raw.Dynamic
+  ( -- * Functions
+    withWGPU,
+  )
+where
+
+import WGPU.Raw.Generated.Fun (WGPUHsInstance, loadDynamicInstance)
+
+#ifdef WGPUHS_UNIX
+import System.Posix.DynamicLinker (withDL, dlsym)
+
+-- | Load WGPU from a dynamic library and run a program using an instance.
+withWGPU ::
+  -- | Path to the wgpu-native dynamic library to load.
+  FilePath ->
+  -- | IO action using an instance of the WGPUHsInstance.
+  (WGPUHsInstance -> IO a) ->
+  -- | Completed IO action.
+  IO a
+withWGPU dynlibFile action = do
+  withDL dynlibFile [] $ \dl -> loadDynamicInstance (dlsym dl) >>= action
+#endif
+
+#ifdef WGPUHS_WINDOWS
+import Foreign (castPtrToFunPtr)
+import System.Win32.DLL (loadLibrary, freeLibrary, getProcAddress)
+
+-- | Load WGPU from a dynamic library and run a program using an instance.
+withWGPU ::
+  -- | Path to the wgpu-native dynamic library to load.
+  FilePath ->
+  -- | IO action using an instance of the WGPUHsInstance.
+  (WGPUHsInstance -> IO a) ->
+  -- | Completed IO action.
+  IO a
+withWGPU dynlibFile action = do
+  -- TODO: safer bracketing
+  hInstance <- loadLibrary dynlibFile
+  let load :: String -> IO (FunPtr a)
+      load = castPtrToFunPtr <$> getProcAddress
+  wgpuHsInstance <- loadDynamicInstance load
+  action wgpuHsInstance
+  freeLibrary hInstance
+#endif
